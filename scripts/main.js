23b1ab4968772cbed4e1a92a16d93480baff9d54
Samuel <otacon100@hotmail.com>
2019-02-25 20:33:47 +0000

    Rendered charts

--- scripts/main.js
+++ scripts/main.js
@@ -1,94 +1,114 @@
 queue()
-    .defer(d3.csv, "data/201801HertfordshireStreet.csv")
+    .defer(d3.csv, "data/2018-01/2018-01-hertfordshire-street.csv")
+    .defer(d3.json, "data/hertfordshire.json")
     .await(makeGraphs);
 
-function makeGraphs(error, crimeData) {
+function makeGraphs(error, crimeData, mapJson) {
     var ndx = crossfilter(crimeData);
 
-    crime_location(ndx);
-    crime_type(ndx);
-    crime_area(ndx);
-    crime_outcome(ndx);
+    crimeType(ndx);
+    crimeArea(ndx);
+    crimeOutcome(ndx);
+    totalCrime(ndx);
+    crimeMap(ndx, mapJson);    
 
     dc.renderAll();
 }
 
-function crime_type(ndx) {
-    var type_dim = ndx.dimension(dc.pluck('Crime type'));
-    var type_group = type_dim.group();
+//Create map of hertfordshire
+function crimeMap(ndx, mapJson) {
+
+    var mapRegion = dc.geoChoroplethChart("#crimeMap");    
+    var regions = ndx.dimension(dc.pluck('LSOA name', function(d){return d.split(" 0");}));
+    var crimeSum = regions.group().reduceSum(function(d){ return d.crime;});
+
+    mapRegion
+        .width(600)
+        .height(600)
+        .dimension(regions)
+        // .projection()
+        .group(crimeSum)
+        .overlayGeoJson(mapJson.features, "region", function(d) {
+            console.log(d.properties.lad17nm);
+            return d.properties.lad17nm;
+        }); 
+}
+
+// Total crime in number format
+function totalCrime(ndx) {
+    var totalGroup = ndx.groupAll();
+
+    dc.numberDisplay('#totalCrime')
+        .group(totalGroup)
+        .formatNumber(d3.format(".0f"))
+        .valueAccessor( function(d) { return d; } )
+        .html({
+            one:'%number crime',
+            some:'%number Crimes',
+            none:'no records'});
+};
+
+// Type of crime in pie chart format
+function crimeType(ndx) {
+    var typeDim = ndx.dimension(dc.pluck('Crime type'));
+    var typeGroup = typeDim.group();
 
     dc.pieChart('#crimeType')
-        .height(430)
-        .radius(290)
+        .width(550)
+        .height(550)
+        .radius(150)
+        .innerRadius(65)
+        .externalRadiusPadding(50)
+        .transitionDuration(1500)        
+        .colors(d3.scale.category20())
+        .dimension(typeDim)
+        .group(typeGroup)
+        .renderLabel(false)
+        .legend(dc.legend().x(400).y(125).itemHeight(15).gap(5));
+};
+
+// Outcome of crime in pie chart format
+function crimeOutcome(ndx) {    
+    var outcomeDim = ndx.dimension(dc.pluck('Last outcome category'));
+    var outcomeGroup = outcomeDim.group();
+
+    dc.pieChart('#crimeOutcome')
+        .width(550)
+        .height(550)
+        .radius(150)
+        .innerRadius(65)
+        .externalRadiusPadding(50)
         .transitionDuration(1500)
-        .dimension(type_dim)
-        .group(type_group);
-}
+        .colors(d3.scale.category10())
+        .dimension(outcomeDim)
+        .group(outcomeGroup)
+        .renderLabel(false)
+        .legend(dc.legend().x(430).y(35).itemHeight(15).gap(5));
+};
 
-function crime_outcome(ndx) {
-    var outcome_dim = ndx.dimension(dc.pluck('Last outcome category'));
-    var outcome_group = outcome_dim.group();
+// Area of crime in bar chart format
+function crimeArea(ndx) {  
+    var areaDim = ndx.dimension(dc.pluck('LSOA name', function(d){
+        return d.split(" 0")[0];
+    }));
+    var areaGroup = areaDim.group();
 
-    dc.barChart("#crimeOutcome")
-        .width(1200)
-        .height(500)
-        .margins({
-            top: 10,
-            right: 50,
-            bottom: 30,
-            left: 50
-        })
-        .dimension(outcome_dim)
-        .group(outcome_group)
-        .transitionDuration(500)
-        .x(d3.scale.ordinal())
-        .xUnits(dc.units.ordinal)
-        .xAxisLabel("Outcome Of Crime")
-        .yAxis().ticks(20);
-}
-
-function crime_location(ndx) {
-    var location_dim = ndx.dimension(dc.pluck('Location'));
-    var location_group = location_dim.group();
-
-    dc.barChart("#crimeLocation")
-        .width(1200)
-        .height(500)
-        .margins({
-            top: 10,
-            right: 50,
-            bottom: 30,
-            left: 50
-        })
-        .dimension(location_dim)
-        .group(location_group)
-        .transitionDuration(500)
-        .x(d3.scale.ordinal())
-        .xUnits(dc.units.ordinal)
-        .xAxisLabel("Location Of Crime")
-        .yAxis().ticks(20);
-}
-
-function crime_area(ndx) {
-    var area_dim = ndx.dimension(dc.pluck('LSOA name'));
-    var area_group = area_dim.group();
     dc.barChart("#crimeArea")
-        .width(1200)
-        .height(500)
+        .width(1100)
+        .height(400)
         .margins({
             top: 10,
             right: 50,
             bottom: 30,
             left: 50
         })
-        .dimension(area_dim)
-        .group(area_group)
+        .dimension(areaDim)
+        .group(areaGroup)
         .transitionDuration(500)
+        .colors(d3.scale.category10())
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .xAxisLabel("Area of crime")
         .elasticY(true)
         .yAxis().ticks(20);
-
-
 }
