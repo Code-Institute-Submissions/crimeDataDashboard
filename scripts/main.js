499115f45b1b4d550036380c6dbdbd85854f0c5e
Samuel <otacon100@hotmail.com>
2019-04-07 20:19:43 +0100

    Fixed select menu option output form 2018-01 to January 2018 etc. Anotated files with appropriate notes

--- scripts/main.js
+++ scripts/main.js
@@ -20,61 +20,62 @@ var opts = {
     position: 'absolute' // Element positioning
 };
 
+// Spinner for loading data
 var target = document.getElementById('loader');
 var spinner = new Spinner(opts).spin(target);
 
 //Queue data and wait for functions to execute and be called 
 queue()
-    .defer(d3.csv, "data/crimeData.csv")
-    .defer(d3.json, "data/hertfordshire.json")
-    .await(makeGraphs);
+    .defer(d3.csv, "data/crimeData.csv") // Crime data
+    .defer(d3.json, "data/hertfordshire.json") // Herts map data
+    .await(makeGraphs); // wait for all graphs to render
 
 // Call all chart functions
 function makeGraphs(error, crimeData, mapJson) {
-    var ndx = crossfilter(crimeData);
+    var ndx = crossfilter(crimeData); // Run all data through crossfilter
 
-    crimeType(ndx);
-    crimeOutcome(ndx);
-    totalCrime(ndx);
-    crimeMap(ndx, mapJson);
-    crimeMonth(ndx);
-    monthTotal(ndx);
+    crimeType(ndx); // Type of crime pie chart
+    crimeOutcome(ndx); // Outcome of crimes row chart
+    totalCrime(ndx); // The total crimes selected, all by default
+    crimeMap(ndx, mapJson); // Map of Hertfordshire
+    crimeMonth(ndx); // Month selector
+    monthTotal(ndx); // Total crimes per month in bar chart
 
-    dc.renderAll();
+    dc.renderAll(); // Render all the charts
 
     // Stop spinner and hidde overlay
     spinner.stop();
     document.getElementById('loader').style.visibility = 'hidden';
 }
 
-//Variable to condense region name from csv file
+//Variable to condense region name from csv file, removing addiitonal numbers
 var area = dc.pluck('LSOA name', function (d) {
     return d.split(" 0")[0];
 });
 
 // Color for chart colours
-var colorRow = d3.scale.ordinal().range(['#355c7d', '#525f7d', '#6b637e', '#80667e', '#95697f', '#aa6b7f', '#bc6e7f', '#d16f80', '#e27180', '#f67280'].reverse());
+var colorRow = d3.scale.ordinal().range(['#355c7d', '#525f7d', '#6b637e', '#80667e', '#95697f', '#aa6b7f', '#bc6e7f', '#d16f80', '#e27180', '#f67280'].reverse()); // Set the color scale and reverse it
 var colorMap = d3.scale.quantize().range(['#355c7d', '#525f7d', '#6b637e', '#80667e', '#95697f', '#aa6b7f', '#bc6e7f', '#d16f80', '#e27180', '#f67280']);
 var colorPie = d3.schemeAccent;
 
-// Total crime in number format
+// Total crime in number format, to display at the top of the page
 function totalCrime(ndx) {
-    var totalGroup = ndx.groupAll();
+    var totalGroup = ndx.groupAll(); // Group all the data
 
     dc.numberDisplay('#totalCrime')
         .group(totalGroup)
-        .formatNumber(d3.format(".0f"))
+        .formatNumber(d3.format(",")) // Format the number into comma seperated sections
         .valueAccessor(function (d) {
             return d;
         })
-        .html({
+        .html({ // Set the display of the outcome, dependant of how many values are returned
             one: '%number Total Reported Crime',
             some: '%number Total reported Crimes',
             none: 'No Total To Display.'
         });
 }
 
-// Create Month selector form data
+// Create a selector of Months for the whole year
 function crimeMonth(ndx) {
     var monthDim = ndx.dimension(dc.pluck('Month'));
     var monthGroup = monthDim.group();
@@ -83,27 +84,28 @@ function crimeMonth(ndx) {
         .dimension(monthDim)
         .group(monthGroup)
         .title(function (d) {
-            return d.key.split("-")[1] + " 2018";
+            var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            var month = d.key.split("-")[1];
+            return (months[month - 1] + " 2018"); // Convert output from "2018-01" to January 2018
         })
-        .multiple(true)
-        .numberVisible(13)
+        .multiple(true) // Multiple outputs to be rendered
+        .numberVisible(13) // Amount of visible outputs, shows all months
         .promptText('All months')
         .useViewBoxResizing(true);
 }
 
 //Create map of hertfordshire
 function crimeMap(ndx, mapJson) {
-    var mapRegion = dc.geoChoroplethChart("#crimeMap");
+    var mapRegion = dc.geoChoroplethChart("#crimeMap"); //Chart
     var regions = ndx.dimension(area);
     var crimeSum = regions.group();
-
-    var width = document.getElementById("one").offsetWidth;
-    var height = 400;
-    var centre = d3.geo.centroid(mapJson);
-    var middle = width / 2;
-    var hMiddle = height / 1.90;
-    var scale = 25000 + (middle + width);
-    var projection = d3.geo.mercator().center(centre).scale(scale).translate([middle, hMiddle]);
+    var width = document.getElementById("one").offsetWidth; // Set the width to the continer the map is loaded to, sets the width properly on page load/refresh
+    var height = 400; // Set solid height
+    var centre = d3.geo.centroid(mapJson); // Center of map
+    var middle = width / 2; // calculate where to position the map horizontaly within the container
+    var hMiddle = height / 1.90; // calculate where to position the map verticaly within the container
+    var scale = 25000 + (middle + width); // Scale the map to "zoom in" to hertfordshire
+    var projection = d3.geo.mercator().center(centre).scale(scale).translate([middle, hMiddle]); // Ensure projection is suitable to display the UK/Europe
 
     mapRegion
         .width(width)
@@ -122,7 +124,7 @@ function crimeMap(ndx, mapJson) {
             mapRegion
                 .width(width)
                 .height(height)
-                .colorDomain([min, max]);
+                .colorDomain([min, max]); // Ensure the map dispays the correct colors for Hi and Lo values when rendered
         })
         .on('preRender', function () {
             var max = crimeSum.top(1)[0].value;
@@ -131,7 +133,7 @@ function crimeMap(ndx, mapJson) {
             mapRegion
                 .width(width)
                 .height(height)
-                .colorDomain([min, max]);
+                .colorDomain([min, max]); // Ensure the map dispays the correct colors for Hi and Lo values when pre render
         })
         .on('preRedraw', function () {
             var max = crimeSum.top(1)[0].value;
@@ -140,12 +142,12 @@ function crimeMap(ndx, mapJson) {
             mapRegion
                 .width(width)
                 .height(height)
-                .colorDomain([min, max]);
+                .colorDomain([min, max]); // Ensure the map dispays the correct colors for Hi and Lo values when pre redraw
         })
         .overlayGeoJson(mapJson.features, "region", function (d) {
             return d.properties.lad17nm;
         })
-        .useViewBoxResizing(true);
+        .useViewBoxResizing(true); // Allow the chart to scale when the screen size changes
 }
 
 // Type of crime in pie chart format
@@ -154,8 +156,7 @@ function crimeType(ndx) {
     var height = width / 2;
     var typeDim = ndx.dimension(dc.pluck('Crime type'));
     var typeGroup = typeDim.group();
-
-    var middle = width / 2;
+    var middle = width / 2; // Calculate where middle of the container
 
     dc.pieChart('#crimeType')
         .width(width)
@@ -172,7 +173,7 @@ function crimeType(ndx) {
         .title(function (d) {
             return d.value;
         })
-        .legend(dc.legend().x(middle).y(10).itemHeight(height / 20).gap(3))
+        .legend(dc.legend().x(middle).y(10).itemHeight(height / 20).gap(3)) // Set the legend to dislay somewhat responsivly.
         .useViewBoxResizing(true);
 }
 
@@ -193,7 +194,7 @@ function crimeOutcome(ndx) {
         .colors(colorRow)
         .gap(1.2)
         .elasticX(true)
-        .cap(9)
+        .cap(9) // Set a cap on displayed data
         .useViewBoxResizing(true);
 }
 
@@ -207,7 +208,7 @@ function monthTotal(ndx) {
     chart
         .width(width)
         .height(width / 2)
-        .margins({
+        .margins({ // Set bar chart margins
             top: 10,
             right: 50,
             bottom: 30,
@@ -216,12 +217,16 @@ function monthTotal(ndx) {
         .ordinalColors(['#f67280', '#355c7d'])
         .dimension(monthDim)
         .group(monthGroup)
+        .on('renderlet', (function (chart) {
+            // rotate x-axis labels
+            chart.selectAll('g.x text')
+                .attr('transform', 'translate(-20,5) rotate(340)'); // Rotate the xAxis labels so they do not overlap
+        }))
         .transitionDuration(500)
-        .renderHorizontalGridLines(true)
+        .renderHorizontalGridLines(true) // Render grid lines for ease of reading
         .useViewBoxResizing(true)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
-        .xAxisLabel("Outcome Of Month")
         .elasticY(true)
         .yAxis().ticks(10);
 }
